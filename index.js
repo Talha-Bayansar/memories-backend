ea89933aeb27098c4d202f3270f37a28b133f46d
Talha Bayansar <talha.bayansar@hotmail.com>
2021-01-16 13:06:14 +0100

    installed dotenv

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+
+dotenv.config();
 
 app.use(cors());
 
