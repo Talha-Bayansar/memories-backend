6176fd675b23cbb08701ceaf5198d7fde854a385
Talha Bayansar <talha.bayansar@hotmail.com>
2021-01-19 16:52:19 +0100

    fixed file upload

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,17 +1,5 @@
 import PostMessage from "../models/postMessage.js";
 import mongoose from "mongoose";
-import multer from "multer";
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage }).single("selectedFile");
 
 export const getPosts = async (req, res) => {
   try {
@@ -23,47 +11,15 @@ export const getPosts = async (req, res) => {
 };
 
 export const createPost = async (req, res) => {
-  console.log(req.body);
-  upload(req, res, function (err) {
-    console.log(req);
-    if (err instanceof multer.MulterError) {
-      return res.status(500).json(err);
-    } else if (err) {
-      return res.status(500).json(err);
-    }
-
-    // const newPost = new PostMessage({
-    //   title: req.body.title,
-    //   message: req.body.message,
-    //   creator: req.body.creator,
-    //   tags: req.body.tags,
-    //   selectedFile: req.files.selectedFile,
-    // });
-
-    // newPost
-    //   .save()
-    //   .then(() => res.json(newPost))
-    //   .catch((error) => res.status(400).json({ message: error }));
-
-    //   // const newPost = new PostMessage({
-    //   //   ...req.body,
-    //   //   selectedFile: req.file.file,
-    //   // });
-    //   // newPost
-    //   //   .save()
-    //   //   .then(() => res.json(newPost))
-    //   //   .catch((error) => res.status(400).json({ message: error }));
-  });
-  console.log(req.files.selectedFile);
-  // const post = req.body;
-  // console.log(post);
-  // const newPost = new PostMessage(post);
-  // try {
-  //   await newPost.save();
-  //   res.status(201).json(newPost);
-  // } catch (error) {
-  //   res.status(409).json({ message: error.message });
-  // }
+  const post = req.body;
+  console.log(post);
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };
 
 export const deletePost = async (req, res) => {
--- index.js
+++ index.js
@@ -4,14 +4,12 @@ import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
 import dotenv from "dotenv";
-import fileUpload from "express-fileupload";
 
 const app = express();
 
 dotenv.config();
 
 app.use(cors());
-app.use(fileUpload());
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
